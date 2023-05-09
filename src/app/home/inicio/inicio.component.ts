b5556122d22a3964590247870191237234f01270
ramses indaldecio <ramsesindaldecio@Mac-mini-de-ramses.local>
2023-05-09 15:34:35 -0700

    agregada la funcion para enviar el formulario

--- src/app/home/inicio/inicio.component.ts
+++ src/app/home/inicio/inicio.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import {HttpClient} from '@angular/common/http'
+import { FormGroup, FormBuilder, Validators, } from '@angular/forms';
 
 @Component({
   selector: 'app-inicio',
@@ -6,5 +8,31 @@ import { Component } from '@angular/core';
   styleUrls: ['./inicio.component.css']
 })
 export class InicioComponent {
+  contactForm: FormGroup | undefined;
+  
+  constructor(private fb: FormBuilder, private http: HttpClient){
+    this.contactForm = this.fb.group({
+      name: ['', Validators.required],
+      email: ['', [Validators.required,Validators.email]],
+      message: ['', Validators.required]
+    });
+  }
 
+  onSubmit() {
+    const url = '/'; // La URL del sitio web de Netlify
+    const formData = new FormData();
+
+    Object.keys(this.contactForm!.controls).forEach(key => {
+      formData.append(key, this.contactForm!.controls[key].value);
+    });
+
+    this.http.post(url, formData).subscribe(() => {
+      console.log('Form submitted successfully');
+      this.contactForm!.reset();
+    }, (error) => {
+      console.log('Form submission failed:', error);
+    });
+  }
+ 
+  
 }
