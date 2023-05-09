c2f357c438caf1f160d203bc1d860c36b2b2ee5a
ramses indaldecio <ramsesindaldecio@Mac-mini-de-ramses.local>
2023-05-08 21:10:36 -0700

    agregado enlace a la prueba1

--- src/app/ejercicios/ejercicios-routing.module.ts
+++ src/app/ejercicios/ejercicios-routing.module.ts
@@ -1,9 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { LeyaoutEjerciciosComponent } from './leyaout-ejercicios/leyaout-ejercicios.component';
+import { Prueba1Component } from './prueba1/prueba1.component';
 
 const routes: Routes = [
-  {path: 'ejercicios', component: LeyaoutEjerciciosComponent }
+  {path: 'ejercicios', component: LeyaoutEjerciciosComponent },
+  {path: 'prueba1', component: Prueba1Component},
 ];
 
 @NgModule({
--- src/app/ejercicios/ejercicios.module.ts
+++ src/app/ejercicios/ejercicios.module.ts
@@ -3,18 +3,21 @@ import { CommonModule } from '@angular/common';
 
 import { EjerciciosRoutingModule } from './ejercicios-routing.module';
 import { LeyaoutEjerciciosComponent } from './leyaout-ejercicios/leyaout-ejercicios.component';
+import { Prueba1Component } from './prueba1/prueba1.component';
 
 
 @NgModule({
   declarations: [
-    LeyaoutEjerciciosComponent
+    LeyaoutEjerciciosComponent,
+    Prueba1Component
   ],
   imports: [
     CommonModule,
     EjerciciosRoutingModule
   ],
   exports: [
-    LeyaoutEjerciciosComponent
+    LeyaoutEjerciciosComponent,
+    Prueba1Component,
   ]
 })
 export class EjerciciosModule { }
