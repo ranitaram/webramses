cb831e80e9996452a807c98054aabc7b44b25991
ramses indaldecio <ramsesindaldecio@Mac-mini-de-ramses.local>
2023-05-14 21:17:27 -0700

    resolviendo el ejercicio 4 a la mitad voy

--- src/app/asistente/tomadedesisiones/tomadedesisiones.component.ts
+++ src/app/asistente/tomadedesisiones/tomadedesisiones.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import * as $ from 'jquery';
+// import * as $ from 'jquery';
 
 @Component({
   selector: 'app-tomadedesisiones',
@@ -7,29 +7,29 @@ import * as $ from 'jquery';
   styleUrls: ['./tomadedesisiones.component.css']
 })
 export class TomadedesisionesComponent {
-  title = 'Asistente de decisiones';
+  // title = 'Asistente de decisiones';
   
 
   constructor() { }
 
-  ngOnInit(): void {
-    $('form').submit(function(event: { preventDefault: () => void; }) {
-      event.preventDefault();
-      var question = $('#question').val();
-      $.ajax({
-        url: 'URL_DEL_SERVICIO',
-        method: 'POST',
-        contentType: 'application/json',
-        data: JSON.stringify({ question: question }),
-        success: function(response: { recommendation: any; }) {
-          $('.recommendation p').text(response.recommendation);
-          $('.recommendation').show();
-        },
-        error: function() {
-          alert('Ha ocurrido un error al procesar la solicitud.');
-        }
-      });
-    });
-  }
+  // ngOnInit(): void {
+  //   $('form').submit(function(event: { preventDefault: () => void; }) {
+  //     event.preventDefault();
+  //     var question = $('#question').val();
+  //     $.ajax({
+  //       url: 'URL_DEL_SERVICIO',
+  //       method: 'POST',
+  //       contentType: 'application/json',
+  //       data: JSON.stringify({ question: question }),
+  //       success: function(response: { recommendation: any; }) {
+  //         $('.recommendation p').text(response.recommendation);
+  //         $('.recommendation').show();
+  //       },
+  //       error: function() {
+  //         alert('Ha ocurrido un error al procesar la solicitud.');
+  //       }
+  //     });
+  //   });
+  // }
 
 }
--- src/app/ejercicios/prueba4/prueba4.component.ts
+++ src/app/ejercicios/prueba4/prueba4.component.ts
@@ -6,5 +6,44 @@ import { Component } from '@angular/core';
   styleUrls: ['./prueba4.component.css']
 })
 export class Prueba4Component {
+  //VARIABLES
+  palabraSecreta: string = "angular";
+  letrasDisponibles: string[] = "abcdefghijklmnopqrstuvwxyz".split("");
+  letrasUsadas: string[] = [];
+  intentos: number = 6;
 
-}
+  get palabraOculta(): string {
+    return this.palabraSecreta
+      .split("")
+      .map(letra => (this.letrasUsadas.includes(letra) ? letra : "_"))
+      .join(" ");
+  }
+
+  seleccionarLetra(letra: string) {
+    this.letrasUsadas.push(letra);
+    if (!this.palabraSecreta.includes(letra)) {
+      this.intentos--;
+    }
+  }
+
+  verificarPerdida() {
+    return this.intentos === 0;
+  }
+
+  verificarGanada() {
+    return !this.palabraOculta.includes("_");
+  }
+
+  iniciarJuego() {
+    this.palabraSecreta = "angular";
+    this.letrasUsadas = [];
+    this.intentos = 6;
+  }
+    
+
+  }
+
+  
+
+  
+
