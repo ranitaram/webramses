fb23f8fe762042e9264ce72a1a3610e38b061bd0
ramses indaldecio <ramsesindaldecio@Mac-mini-de-ramses.local>
2023-05-28 10:06:55 -0700

    validaciones de formulario

--- src/app/auth/register/register.component.ts
+++ src/app/auth/register/register.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormBuilder, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-register',
@@ -6,5 +7,43 @@ import { Component } from '@angular/core';
   styleUrls: ['./register.component.css']
 })
 export class RegisterComponent {
+
+  //para saber cuando el formulario se envio o no
+  public formSubmitted = false;
+
+  public registerForm = this.fb.group({
+    nombre: ['', [Validators.required, Validators.minLength(3)]],
+    email: ['', [Validators.required, Validators.email]],
+    password: ['', Validators.required],
+    password2: ['', Validators.required],
+    terminos: [false, Validators.required],
+  });
+
+  constructor(private fb: FormBuilder){}
+
+  //metodo para capturar la informacion
+  crearUsuario(){
+    this.formSubmitted = true;
+    console.log(this.registerForm.value);
+
+    if (this.registerForm.valid) {
+      console.log('postenado formulario')
+    }else {
+      console.log('Formulario no es correcto....');
+    }
+  }
+
+  campoNoValido(campo: string):boolean{
+    if (this.registerForm.get(campo)?.invalid && this.formSubmitted) {
+      return true;
+    }else {
+      return false;
+    }
+  }
+
+  aceptarTerminos(){
+    return !this.registerForm.get('terminos')?.value && this.formSubmitted;
+
+  }
 
 }
