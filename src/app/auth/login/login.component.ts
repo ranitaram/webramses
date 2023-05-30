0afcb9f0237271def4d3eb7baa5073a9a231ac71
ramses indaldecio <ramsesindaldecio@Mac-mini-de-ramses.local>
2023-05-30 10:21:34 -0700

    obteniendo el token de google sign in

--- src/app/auth/login/login.component.ts
+++ src/app/auth/login/login.component.ts
@@ -1,14 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, AfterViewInit, ViewChild, ElementRef } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { UsuarioService } from 'src/app/services/usuario.service';
 import Swal from 'sweetalert2';
+
+declare const google: any;
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.css']
 })
-export class LoginComponent {
+export class LoginComponent implements AfterViewInit{
+
+  @ViewChild('googleBtn')
+  googleBtn!: ElementRef;
+
+
    //para saber cuando el formulario se envio o no
    public formSubmitted = false;
    loginForm: FormGroup;
@@ -18,20 +25,43 @@ export class LoginComponent {
     private usuarioService: UsuarioService){
       this.loginForm = this.fb.group({
     
-        email: ['', [Validators.required, Validators.email]],
+        email: [localStorage.getItem('email') || '', [Validators.required, Validators.email]],
         password: ['', Validators.required],
         recordar: [false]
        
       });
     }
 
+    ngAfterViewInit(): void {
+      this.googleInit();
+    }
+
+    googleInit(){
+      google.accounts.id.initialize({
+        client_id: "44276019668-9fcdqcd4gaenk3f1b5qkkk8e4a8376q4.apps.googleusercontent.com",
+        callback: this.handleCredentialResponse
+      });
+      google.accounts.id.renderButton(
+       // document.getElementById("buttonDiv"),
+       this.googleBtn.nativeElement,
+        { theme: "outline", size: "large" }  // customization attributes
+      );
+    }
+
+    handleCredentialResponse(response: any){
+      console.log("Encoded JWT ID token: " + response.credential);
+    }
 
     login(){
       this.usuarioService.login(this.loginForm.value)
       .subscribe(
         {
           next: resp => {
-            console.log(resp);
+            if (this.loginForm.get('recordar')?.value){
+             localStorage.setItem('email', this.loginForm.get('email')?.value)
+            }else {
+              localStorage.removeItem('email');
+            }
           },
           error: err => {
             Swal.fire('Error', err.error.msg, 'error')
