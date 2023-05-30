1d2dc8e86dcbcc276d7e84d5d8ebac3a1fa710d2
ramses indaldecio <ramsesindaldecio@Mac-mini-de-ramses.local>
2023-05-29 18:45:55 -0700

    login del usaurio normal

--- src/app/auth/login/login.component.ts
+++ src/app/auth/login/login.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { UsuarioService } from 'src/app/services/usuario.service';
+import Swal from 'sweetalert2';
 
 @Component({
   selector: 'app-login',
@@ -6,5 +9,34 @@ import { Component } from '@angular/core';
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent {
+   //para saber cuando el formulario se envio o no
+   public formSubmitted = false;
+   loginForm: FormGroup;
+   
+ 
+   constructor(private fb: FormBuilder,
+    private usuarioService: UsuarioService){
+      this.loginForm = this.fb.group({
+    
+        email: ['', [Validators.required, Validators.email]],
+        password: ['', Validators.required],
+        recordar: [false]
+       
+      });
+    }
 
+
+    login(){
+      this.usuarioService.login(this.loginForm.value)
+      .subscribe(
+        {
+          next: resp => {
+            console.log(resp);
+          },
+          error: err => {
+            Swal.fire('Error', err.error.msg, 'error')
+          }
+        }
+      )
+    }
 }
--- src/app/services/usuario.service.ts
+++ src/app/services/usuario.service.ts
@@ -2,6 +2,7 @@ import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { RegisterForm } from '../interfaces/register-form.interface';
 import { GLOBAL } from './GLOBAL';
+import { LoginForm } from '../interfaces/login-form.interface';
 
 const base_url = GLOBAL.url;
 
@@ -14,5 +15,9 @@ export class UsuarioService {
 
   crearUsuario(formData: RegisterForm){
     return this.http.post(`${base_url}usuarios/crear`, formData);
+  }
+
+  login(formData: LoginForm){
+    return this.http.post(`${base_url}login`,formData);
   }
 }
