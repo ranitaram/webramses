420f4095fff059f33d2caa4a8353f6f17a3a21d0
ramses indaldecio <ramsesindaldecio@Mac-mini-de-ramses.local>
2023-06-11 19:24:37 -0700

    agregado el componente de los precios

--- src/app/components/components.module.ts
+++ src/app/components/components.module.ts
@@ -1,18 +1,21 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ModalComponent } from './modal/modal.component';
+import { PreciosComponent } from './precios/precios.component';
 
 
 
 @NgModule({
   declarations: [
-    ModalComponent
+    ModalComponent,
+    PreciosComponent
   ],
   imports: [
     CommonModule
   ],
   exports: [
     ModalComponent,
+    PreciosComponent
   ]
 })
 export class ComponentsModule { }
