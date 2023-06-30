be690ba6fe62a1369de1dfed6175edb83a3ba342
ramses indaldecio <ramsesindaldecio@Mac-mini-de-ramses.local>
2023-06-30 08:31:38 -0700

    ejercicio 7 terminado

--- src/app/ejercicios/prueba7/prueba7.component.ts
+++ src/app/ejercicios/prueba7/prueba7.component.ts
@@ -5,6 +5,23 @@ import { Component } from '@angular/core';
   templateUrl: './prueba7.component.html',
   styleUrls: ['./prueba7.component.css']
 })
+//crear un programa que cuente las palabras de un texto
 export class Prueba7Component {
+
+  texto: string = '';
+  contador: number = 0;
+
+  contarPalabras() {
+    if (this.texto.trim() === '') {
+      this.contador = 0;
+    } else {
+      const palabras = this.texto.trim().split(' ');
+      this.contador = palabras.length;
+    }
+    
+  }
+
+  
+
 
 }
