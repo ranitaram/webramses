71c26c1747d04a3a34e52c617302207b7d45d194
ramses indaldecio <ramsesindaldecio@Mac-mini-de-ramses.local>
2023-07-01 08:45:08 -0700

    simulacion de escritura, falta cambiar de color los signos

--- src/app/ejercicios/prueba8/prueba8.component.ts
+++ src/app/ejercicios/prueba8/prueba8.component.ts
@@ -6,5 +6,32 @@ import { Component } from '@angular/core';
   styleUrls: ['./prueba8.component.css']
 })
 export class Prueba8Component {
+  textoUsuario: string = '';
+  textoSimulado: string = '';
 
+  simularEscritura() {
+    const velocidadEscritura = 50; 
+    const texto = this.textoUsuario;
+    let index = 0;
+
+    this.textoSimulado = '';
+
+    const escribirTexto = () => {
+      const letra = texto.charAt(index);
+      this.textoSimulado += letra;
+      index++;
+
+      const textareaSimulado = document.getElementById('textoSimulado');
+      if (textareaSimulado) {
+        textareaSimulado.innerText += letra;
+        textareaSimulado.scrollTop = textareaSimulado.scrollHeight;
+      }
+
+      if (index < texto.length) {
+        setTimeout(escribirTexto, velocidadEscritura);
+      }
+    };
+
+    escribirTexto();
+  }  
 }
